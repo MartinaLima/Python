c335effb49884d8082c70b2e7254062b64abbd56
MartinaLima <73909451+MartinaLima@users.noreply.github.com>
2021-01-08 15:43:10 -0300

    Add files via upload

---
+++ exercicios_python_brasil/estrutura_decisao/03_f_ou_m.py
@@ -0,0 +1,7 @@
+sexo = str(input('Escolha o sexo [F/M]: '))
+if sexo in 'Ff':
+    print('Você escolheu sexo FEMININO!')
+elif sexo in 'Mm':
+    print('Você escolheu sexo MASCULINO!')
+else:
+    print('SEXO INVÁLIDO!')
