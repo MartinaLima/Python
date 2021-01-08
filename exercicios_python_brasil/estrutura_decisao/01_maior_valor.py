01834f9500bbdd28d69fb39315952d303aea4676
MartinaLima <73909451+MartinaLima@users.noreply.github.com>
2021-01-08 15:35:13 -0300

    Add files via upload

---
+++ exercicios_python_brasil/estrutura_decisao/01_maior_valor.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+if n1 > n2:
+    print(f'>> O maior valor é {n1}.')
+elif n1 < n2:
+    print(f'>> O maior valor é {n2}.')
+else:
+    print('>> Os valores são iguais.')
