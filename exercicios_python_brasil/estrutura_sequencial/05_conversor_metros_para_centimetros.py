486bfd06ee6a147bbed0fce3d5bb78caac0d840a
MartinaLima <73909451+MartinaLima@users.noreply.github.com>
2021-01-08 09:27:43 -0300

    Add files via upload

---
+++ exercicios_python_brasil/estrutura_sequencial/05_conversor_metros_para_centimetros.py
@@ -0,0 +1,5 @@
+print('\033[1mCONVERSOR DE METROS EM CENTÍMETROS\033[m')
+print('º'*35)
+m = float(input('Informe a medida em metros: '))
+c = m*100
+print(f'\033[1mRESULTADO:\033[m\n{m}m é igual a {c:.2f}cm.')
