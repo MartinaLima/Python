b3a0554c58027f78d6b127a6d04b2822d6c13697
MartinaLima <73909451+MartinaLima@users.noreply.github.com>
2021-01-08 10:04:27 -0300

    Add files via upload

---
+++ exercicios_python_brasil/estrutura_sequencial/07_dobro_area_quadrado.py
@@ -0,0 +1,7 @@
+print('\033[1m>>> ÁREA DO QUADRADO <<<\033[m')
+l = float(input('Informe o lado do quadrado (m): '))
+area = l*l
+dobro = area*2
+print('-'*30)
+print(f'ÁREA DO QUADRADO: \033[1m{area:.2f}m²\033[m.')
+print(f'DOBRO DA ÁREA DO QUADRADO: \033[1m{dobro:.2f}.')
