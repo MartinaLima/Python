3cecf6e785ed26d35c671a849745c86b2d4e21c8
MartinaLima <73909451+MartinaLima@users.noreply.github.com>
2021-01-08 09:17:29 -0300

    Add files via upload

---
+++ exercicios_python_brasil/estrutura_sequencial/04_media_4_notas.py
@@ -0,0 +1,10 @@
+print('-'*40)
+print('{:^45}'.format('\033[1mMÉDIA ARITMÉTICA\033[m'))
+print('-'*40)
+print('INFORME AS 4 NOTAS DO ALUNO!')
+n1 = float(input('Nota 1: '))
+n2 = float(input('Nota 2: '))
+n3 = float(input('Nota 3: '))
+n4 = float(input('Nota 4: '))
+media = (n1+n2+n3+n4)/4
+print(f'-> A média do aluno é igual a \033[1m{media:.1f}')
