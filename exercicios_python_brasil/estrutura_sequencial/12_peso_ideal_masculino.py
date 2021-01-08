cbb0db1bd1b435094243b4c032bd9a92f7b37ef4
MartinaLima <73909451+MartinaLima@users.noreply.github.com>
2021-01-08 13:47:30 -0300

    Add files via upload

---
+++ exercicios_python_brasil/estrutura_sequencial/12_peso_ideal_masculino.py
@@ -0,0 +1,4 @@
+print('\033[1m>>> PESO IDEAL MASCULINO <<<\033[m')
+a = float(input('INFORME A ALTURA (m): '))
+peso = (72.7*a)-58
+print(f'\033[1m\033[4mRESULTADO\033[m:\nPESO IDEAL: \033[1:33m{peso:.2f}kg')
