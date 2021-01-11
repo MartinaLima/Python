68184eab50e548b126b5d786d2616e2e5ce941a9
MartinaLima <73909451+MartinaLima@users.noreply.github.com>
2021-01-11 13:57:54 -0300

    Add files via upload

---
+++ exercicios_python_brasil/estrutura_decisao/13_dia_semana.py
@@ -0,0 +1,17 @@
+num = int(input('>>> DIGITE O NÚMERO DO DIA DA SEMANA (1 A 7): '))
+if num == 1:
+    print('DOMINGO!')
+elif num == 2:
+    print('SEGUNDA-FEIRA!')
+elif num == 3:
+    print('TERÇA-FEIRA!')
+elif num == 4:
+    print('QUARTA-FEIRA!')
+elif num == 5:
+    print('QUINTA-FEIRA!')
+elif num == 6:
+    print('SEXTA-FEIRA')
+elif num == 7:
+    print('SÁBADO!')
+else:
+    print('INVÁLIDO!')
