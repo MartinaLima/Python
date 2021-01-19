c7d02d5a77419e1f33675c09bce40269ec951f38
MartinaLima <martina.lima@outlook.com>
2021-01-19 14:08:07 -0300

    Revert "Add files via upload"
    This reverts commit 501081c4c428e3e12861bde5a486c110c0bd7ca0.

--- exercicios_python_brasil/estrutura_repeticao/13_potencia.py
+++
@@ -1,5 +0,0 @@
-print('\033[1m>>> POTÊNCIA <<<\033[m')
-base = int(input('BASE: '))
-expoente = int(input('EXPOENTE: '))
-potencia = base ** expoente
-print(f'--> O resultado da potência é: {potencia}')
