ccb4b39dd0268fe6dd3dc8c505429392756a2601
MartinaLima <73909451+MartinaLima@users.noreply.github.com>
2021-01-19 11:29:55 -0300

    Melhoria no código

--- exercicios_python_brasil/estrutura_repeticao/10_gerador_numeros_intervalo.py
+++ exercicios_python_brasil/estrutura_repeticao/10_gerador_numeros_intervalo.py
@@ -1,11 +1,11 @@
 print('\033[1m>>> CONTAGEM <<<\033[m')
 n1 = int(input('1º VALOR: '))
 n2 = int(input('2º VALOR: '))
-if n2 > n1:
+if n2 > n1 or n1 == n2:
     for c in range(n1 - 1, n2):
         c += 1
         print(c, end=' ')
-elif n1 > n2:
+elif n1 > n2 or n1 == n2:
     for c in range(n2 - 1, n1):
         c += 1
         print(c, end=' ')
