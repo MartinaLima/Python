501081c4c428e3e12861bde5a486c110c0bd7ca0
MartinaLima <73909451+MartinaLima@users.noreply.github.com>
2021-01-19 13:57:34 -0300

    Add files via upload

---
+++ exercicios_python_brasil/estrutura_repeticao/13_potencia.py
@@ -0,0 +1,5 @@
+print('\033[1m>>> POTÊNCIA <<<\033[m')
+base = int(input('BASE: '))
+expoente = int(input('EXPOENTE: '))
+potencia = base ** expoente
+print(f'--> O resultado da potência é: {potencia}')
