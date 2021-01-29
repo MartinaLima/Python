82c013ad9ab4b62569c03de0e15b68a95ce178ca
MartinaLima <73909451+MartinaLima@users.noreply.github.com>
2021-01-29 15:04:49 -0300

    Add files via upload

---
+++ exercicios_diversos/aprovacao_media_porcentagem.py
@@ -0,0 +1,42 @@
+print('\033[1m>>> PESQUISA DE APROVAÇÃO DE ALUNOS <<<\033[m')
+ap = 0
+rep = 0
+rec = 0
+opcao = ''
+alunos = 0
+media_ap = 0
+media_total = 0
+while opcao != 'N':
+    alunos += 1
+    print(f'\033[1mALUNO {alunos}\033[m')
+    n1 = float(input('Nota 1: '))
+    n2 = float(input('Nota 2: '))
+    n3 = float(input('Nota 3: '))
+    media = (n1+n2+n3)/3
+    print(f'Média = {media:.2f}', end=' | ')
+    if media > 7:
+        print('APROVADO!')
+        ap += 1
+        media_ap += media
+    elif media < 6:
+        print('REPROVADO!')
+        rep += 1
+    else:
+        print('RECUPERAÇÃO!')
+        rec += 1
+    media_total += media
+    opcao = str(input('CADASTRAR MAIS ALUNOS? [S/N] ').upper())
+    while opcao not in 'SN':
+        opcao = str(input('CADASTRAR MAIS ALUNOS? [S/N] ').upper())
+if ap >= 1:
+    media_aprovados = media_ap/ap
+else:
+    media_aprovados = 0
+print('-'*40)
+print('\033[1mRESULTADOS...\033[m')
+print(f'* Quantidade de alunos: {alunos}')
+print(f'* Alunos aprovados {(ap*100)/alunos:.1f}%: {ap}')
+print(f'* Alunos reprovados {(rep*100)/alunos:.1f}%: {rep}')
+print(f'* Alunos em recuperação {(rec*100)/alunos:.1f}%: {rec}')
+print(f'* Média geral: {media_total/alunos:.2f}')
+print(f'* Média aprovados: {media_aprovados:.2f}')
