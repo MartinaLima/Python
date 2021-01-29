1d0c08dd103389721a6e28c04cf0745059a0a38f
MartinaLima <73909451+MartinaLima@users.noreply.github.com>
2021-01-29 15:30:16 -0300

    Update aliquota_inss.py

--- exercicios_diversos/aliquota_inss.py
+++ exercicios_diversos/aliquota_inss.py
@@ -1,3 +1,12 @@
+"""Crie um programa que leia o nome completo de um funcionário e seu salário bruto e com base nisso,
+calcule a alíquota a ser descontada pelo INSS. Ao final informe o nome do funcionário, o salário bruto,
+a qual porcentagem de alíquota se encaixa, o valor total descontado pelo INSS e o valor líquido (a receber).
+Considere a seguinte tabela de alíquota do INSS. 
+Até R$ 1751.81 - ALÍQUOTA DE 8% 
+De R$ 1751,82 até R$ 2919.72 - ALÍQUOTA DE 9% 
+De R$ 2919.73 até R$ 5839.44 - ALÍQUOTA DE 11% 
+A partir de R$ 5839.45 - ALÍQUOTA R$ 642,38 """
+
 print('-'*12 + ' DESCONTO INSS ' + '-'*11)
 nome = str(input('NOME FUNCIONÁRIO(A): '))
 bruto = float(input('SALÁRIO BRUTO: R$ '))
