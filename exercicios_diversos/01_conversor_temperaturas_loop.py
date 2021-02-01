0025a30fb932e56c452d68322ff8cb15becb2f6f
MartinaLima <73909451+MartinaLima@users.noreply.github.com>
2021-02-01 07:53:15 -0300

    Update 01_conversor_temperaturas_loop.py

--- exercicios_diversos/01_conversor_temperaturas_loop.py
+++ exercicios_diversos/01_conversor_temperaturas_loop.py
@@ -3,11 +3,11 @@ escolha = ''
 while escolha != 'N':
     temp = float(input('Informe a temperatura: '))
     opcao = int(input('Escolha o tipo de conversão:\n'
-                      '[1] Célsius para Fahrenheit\n'
-                      '[2] Célsius para Kelvin\n'
-                      '[3] Fahrenheit para Célsius\n'
+                      '[1] Celsius para Fahrenheit\n'
+                      '[2] Celsius para Kelvin\n'
+                      '[3] Fahrenheit para Celsius\n'
                       '[4] Fahrenheit para Kelvin\n'
-                      '[5] Kelvin para Célsius\n'
+                      '[5] Kelvin para Celsius\n'
                       '[6] Kelvin para Fahrenheit\n'
                       '---> '))
     tipo1 = ''
@@ -15,16 +15,16 @@ while escolha != 'N':
     resultado = 0
     if opcao in (1, 2, 3, 4, 5, 6):
         if opcao == 1:
-            tipo1 = '° Célsius'
+            tipo1 = '° Celsius'
             tipo2 = '° Fahrenheit'
             resultado = 1.8 * temp + 32
         elif opcao == 2:
-            tipo1 = '° Célsius'
+            tipo1 = '° Celsius'
             tipo2 = ' Kelvin'
             resultado = 273.15 + temp
         elif opcao == 3:
             tipo1 = '° Fahrenheit'
-            tipo2 = '° Célsius'
+            tipo2 = '° Celsius'
             resultado = (temp - 32) / 1.8
         elif opcao == 4:
             tipo1 = '° Fahrenheit'
@@ -32,7 +32,7 @@ while escolha != 'N':
             resultado = ((temp - 32) / 1.8) + 273.15
         elif opcao == 5:
             tipo1 = ' Kelvin'
-            tipo2 = '° Célsius'
+            tipo2 = '° Celsius'
             resultado = temp - 273.15
         elif opcao == 6:
             tipo1 = ' Kelvin'
